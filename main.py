571baef1ea075d68da09df556222633faec1ed89
cameronrc <cam_carley@hotmail.com>
2020-08-22 11:11:44 -0400

    methods to detect if number is in row && column

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import numpy as np
 
+# print (np.matrix(sudoku))
+class board:
 
+    def __init__(self,dimensions):
+        board.matrix = [[0 for i in range(dimensions)] for j in range(dimensions)]
+        pass
 
-def __main__():
+    def inColumn(self,number,columnIndex):
+        for num in range(len(board.matrix[columnIndex])):
+            if number == (board.matrix[num][columnIndex]):
+                return True
+        return False
+        pass
 
-    pass
+    def inRow(self,number,rowIndex):
+        for num in range(len(board.matrix[rowIndex])):
+            if number == (board.matrix[rowIndex][num]):
+                return True
+        return False
+        pass
+
+    def inQuadrant(self, quadrantIndex):
+
+        pass
+
+class game:
+    if __name__ == '__main__':
+    #   dimensions = int(input("What board size would you like to play with?"))
+        dimensions = 9
+        fun = board(dimensions)
+        # print('Hello World')
+        print(np.matrix(board.matrix))
+        print(board.inRow(0,1,0))
