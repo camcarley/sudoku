4b72fa9b6384cb0d40dd5cbcf9ea6744a794de44
cameronrc <cam_carley@hotmail.com>
2020-08-26 18:24:29 -0400

    removed print statements from set methods , implementing solve method (WIP)

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import numpy as np
-import pygame as py 
 
 # print (np.matrix(sudoku))
 class board(object):
-    
+
     def isGameOver(self):
-        for x in range(len(board.matrix)):
-            for y in range(len(board.matrix[x])):
-                if board.matrix[x][y] == 0:
+        for x in range(len(board)):
+            for y in range(len(board[x])):
+                if board[x][y] == 0:
                     return False
         return True
 
     def __init__(self,dimensions):
-        board.matrix = [[0 for i in range(dimensions)] for j in range(dimensions)]
+        
+        board = [[0 for i in range(dimensions)] for j in range(dimensions)]
 
     def setNumber(self,coordX,coordY,number):
-        board.matrix[coordX][coordY] = number
-        print('Move set at %2d,%2d',coordX,coordY)
+        board[coordX][coordY] = number
+        # print('Move set at %2d,%2d',coordX,coordY)
 
     def inColumn(self,number,columnIndex):
-        for num in range(len(board.matrix[columnIndex])):
-            if number == (board.matrix[num][columnIndex]):
-                print('Number already present in column')
+        for num in range(len(board[columnIndex])):
+            if number == (board[num][columnIndex]):
+                # print('Number already present in column')
                 return True
         return False
         
 
     def inRow(self,number,rowIndex):
-        for num in range(len(board.matrix[rowIndex])):
-            if number == (board.matrix[rowIndex][num]):
-                print('Number already present in row')
+        for num in range(len(board[rowIndex])):
+            if number == (board[rowIndex][num]):
+                # print('Number already present in row')
                 return True
         return False
         
@@ -37,17 +37,29 @@ class board(object):
     def inQuadrant(self,number,coordX,coordY):
         coordX = self.calculatePosition(coordX)
         coordY = self.calculatePosition(coordY)
-        print(self.calculatePosition(coordX))
-        print(self.calculatePosition(coordY))
         for x in range(coordX,coordX+3):
             for y in range(coordY,coordY+3):
-                if number == board.matrix[coordX][coordY]:
-                    print('Number already exists in that quadrant')
+                if number == board[coordX][coordY]:
+                    # print('Number already exists in that quadrant')
                     return True
         return False           
     
-    def moveCanOccur(self,number,coordX,coordY):
+    def isMovePossible(self,number,coordX,coordY):
         return not self.inQuadrant(number,coordX,coordY) and not self.inRow(number,coordX) and not self.inColumn(number,coordY)
+
+    def solve(self):
+        for x in range(9):
+            for y in range(9):
+                if board[x][y]==0:
+                    for n in range(1,10):
+                        if self.isMovePossible(n,x,y):
+                            self.setNumber(x,y,n)
+                            self.solve()
+                            #If solution cannot complete , then backtracking will occur
+                            self.setNumber(x,y,0)
+                    return
+
+
 
 
 #I created this function in order to reset the quadrant position within the sudoku board.
@@ -64,18 +76,19 @@ class game:
     def main():
         dimensions = 9
         gameboard = board(dimensions)
-        while not gameboard.isGameOver():
-            while True:
-                print(np.matrix(gameboard.matrix))
-                row = int(input('Enter the X Coordinate: '))
-                col = int(input('Enter the Y coordinate: '))
-                num = int(input('Enter the number: '))
-                if gameboard.moveCanOccur(num,row,col):
-                    gameboard.setNumber(row,col,num)
-                    break
-                else:
-                    print('Error , Try again')
-
+        # while not gameboard.isGameOver():
+        #     while True:
+        #         print(np.matrix(gameboard))
+        #         row = int(input('Enter the X Coordinate: '))
+        #         col = int(input('Enter the Y coordinate: '))
+        #         num = int(input('Enter the number: '))
+        #         if gameboard.isMovePossible(num,row,col):
+        #             gameboard.setNumber(row,col,num)
+        #             break
+        #         else:
+        #             print('Error , Try again')
+        gameboard.solve()
+        print(np.matrix(gameboard))
 
     if __name__ == '__main__':
         main()
