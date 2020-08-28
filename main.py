f3ac053feb79e059335919665f362f86ebcfa331
cameronrc <cam_carley@hotmail.com>
2020-08-28 10:28:20 -0400

    completed solve algo

--- main.py
+++ main.py
@@ -1,116 +1,78 @@
-import numpy as np
-# print (np.matrix(sudoku))
+b = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7] 
+]
 
-class board(object):
+def empty_pos(b):
+    for x in range(len(b)):
+        for y in range(len(b[0])):
+            if b[x][y] == 0:
+                return [x,y]
+    return None
+
+def valid_move(b,num,coords):
+    for j in range(9):
+        if num == b[coords[0]][j]:
+            return False
+        
+    for j in range(9):
+        if num == b[j][coords[1]]:
+            return False
+            
+    coordx = isolate_position(coords[1])
+    coordy = isolate_position(coords[0])
+
+    for i in range(coordy, coordy + 3):
+        for j in range(coordx, coordx + 3):
+            if b[i][j] == num:
+                return False      
+    return True
+
+def isolate_position(num):
+    while num %3 != 0:
+        num = num-1
+    return num
+        
+def print_board(b):
+    print("---------------------") 
+    for z in range(9):
+        if z % 3 == 0 and z != 0:
+            print("---------------------") 
+
+        for q in range(9):
+            if q % 3 == 0 and q != 0:
+                print("|", end="")
+
+            if q == 8:
+                print(b[z][q])
+            else:
+                print(str(b[z][q]) + " ", end="")
+
+    print("---------------------")        
     
-    def __init__(self):
-        board.matrix = [
-            [7,8,0,4,0,0,1,2,0],
-            [6,0,0,0,7,5,0,0,9],
-            [0,0,0,6,0,1,0,7,8],
-            [0,0,7,0,4,0,2,6,0],
-            [0,0,1,0,5,0,9,3,0],
-            [9,0,4,0,6,0,0,0,5],
-            [0,7,0,3,0,0,0,1,2],
-            [1,2,0,0,0,7,4,0,0],
-            [0,4,9,2,0,6,0,0,7]]
-
-
-    def empty_pos(self):
-        for x in range(9):
-            for y in range(9):
-                if board.matrix[x][y] == 0:
-                    print((x,y))
-                    return (x,y)
-        return None
-
-    def in_column(self,num,col):
-        for n in range(9):
-            if num == board.matrix[n][col]:
-                print('Number already present in column')
-                return True
-        return False
-        
-
-    def in_row(self,num,row):
-        for n in range(9):
-            if num == board.matrix[row][n]:
-                print('Number already present in row')
-                return True
-        return False
-        
-
-    def in_quadrant(self,num,x,y):
-        posx = self.isolate_position(x)
-        posy = self.isolate_position(y)
-        for x in range(posx,posx+3):
-            for y in range(posy,posy+3):
-                if num == board.matrix[y][x]:
-                    print('Number already exists in that quadrant')
-                    return True
-        return False           
-    
-    def valid_move(self,num,y,x):
-        if self.in_row(num,x):
-            print('a')
-            return False
-        
-        if self.in_row(num,y):
-            print('b')
-            return False
-
-        if self.in_quadrant(num,x,y):
-            print('c')
-            return False
-
+def solve(b):
+    empty = empty_pos(b)
+    if not empty:
+        print('\n Solved:')
+        print_board(b)
         return True
-        
-        
-    def print_board(self):
-        for z in range(len(board.matrix)):
-            if z % 3 == 0 and z != 0:
-                print("- - - - - - - - - - - - - ")
-
-            for q in range(len(board.matrix[0])):
-                if q % 3 == 0 and q != 0:
-                    print("|", end="")
-
-                if q == 8:
-                    print(board.matrix[z][q])
-                else:
-                    print(str(board.matrix[z][q]) + " ", end="")
-    
-#I created this function in order to reset the quadrant position within the sudoku board.
-#Explained in docs
-    def isolate_position(self,num):
-        return int((num/3)*3)
-
-    def solve(self):
-        find = self.empty_pos()
-        if not find:
-            return True
-        else:
-            posX,posY = find
-            for n in range(1,10):
-                if board.matrix[posY][posX]==0:
-                    if self.valid_move(n,posY,posX):
-                        board.matrix[posY][posX]=n
-                        if self.solve():
-                            return True
-                        board.matrix[posY][posX]=n
-            return False
-
-    
-class game:
-    def __init__(self):
-        pass
-
-    def main():
-        gameboard = board()
-        print(np.matrix(gameboard.matrix))
-        gameboard.solve()
-        gameboard.print_board()
+    else:
+        posX,posY = empty_pos(b)
+        for n in range(1,10):
+            if valid_move(b,n,(posX,posY)):
+                b[posX][posY]=n
+                if solve(b):
+                    return True
+                b[posX][posY]=0
+    return False
 
 
-    if __name__ == '__main__':
-        main()
+print_board(b)
+solve(b)
